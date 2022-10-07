d70dcdcb92b28bff8268a0f9352f224195552966
cedri <cedri@DESKTOP-L6UGAKJ>
2022-10-07 21:04:54 +0200

    ghaa

---
+++ EX-02/src/myIO/Prompt.java
@@ -0,0 +1,16 @@
+package myIO;
+
+import java.util.Scanner;
+
+public class Prompt {
+	public static int PromptInt(String text) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(text);
+		return scanner.nextInt();
+	}
+	public static double PromptDouble(String text) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(text);
+		return scanner.nextDouble();
+	}
+}
--- EX-02/src/shapes/Rectangle.java
+++ EX-02/src/shapes/Rectangle.java
@@ -1,9 +1,43 @@
-
-public class Main {
-
+package shapes;
+import myIO.Prompt;
+public class Rectangle {
+	
+	double lengthX;
+	double lengthY;
+	
+	public Rectangle(double lengthX, double lengthY) {
+		this.lengthX = lengthX;
+		this.lengthY = lengthY;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//test
+		double x = Prompt.PromptDouble("Enter the width of a rectangle");
+		double y = Prompt.PromptDouble("Enter the height of arectangle");
+		
+		Rectangle rect = new Rectangle(x,y);
+		
+		System.out.println(
+				String.format("This rectganles area is %.4f", rect.ComputeArea())
+				);
+		System.out.println(
+				String.format("This rectangles perimeter is %.4f", rect.ComputePerimeter())
+				);
+		if(rect.IsSquare()) {
+			System.out.println("The rectangle you defined is actually a square!");
+		}
+		else {
+			System.out.println("Your rectangle is not a Square!");
+		}		
+	}
+	
+	public boolean IsSquare() {
+		return lengthX == lengthY;
+	}
+	public double ComputeArea() {
+		return lengthX*lengthY;
+	}
+	public double ComputePerimeter(){
+		return 2*lengthX + 2*lengthY;
 	}
 
 }
